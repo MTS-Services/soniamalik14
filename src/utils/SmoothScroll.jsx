11a99abc6740f69727a0607bf9b0217a843d6aa9
Mehedi <mehedi25122003@gmail.com>
2026-01-22 12:00:04 +0600

    smooth scrolling and title responsive issue fixed

--- src/utils/SmoothScroll.jsx
+++ src/utils/SmoothScroll.jsx
@@ -1,59 +1,26 @@
-
-import { useEffect, useRef } from "react";
-import { useLocation } from "react-router-dom";
-import Lenis from "lenis";
-import "lenis/dist/lenis.css";
+import React, { useMemo } from 'react';
+import { ReactLenis } from 'lenis/react';
+import 'lenis/dist/lenis.css';
 
 
-const SmoothScroll = ({ children }) => {
-  const lenisRef = useRef(null);
-  const location = useLocation();
-  const prevPathRef = useRef(location.pathname);
+const SmoothScroll = ({ children, root = true, className = '' }) => {
+  const lenisOptions = useMemo(() => ({
+    lerp: 0.3,
+    duration: 1.5,
+    smoothWheel: true,
+    smoothTouch: false,
+    wheelMultiplier: 1,
+    touchMultiplier: 2,
+    orientation: 'vertical',
+    gestureDirection: 'vertical',
+    infinite: false,
+  }), []);
 
-  useEffect(() => {
-    const lenis = new Lenis({
-      lerp: 0.1,
-      duration: 1.5,
-      smoothWheel: true,
-      smoothTouch: false,
-      wheelMultiplier: 1,
-      touchMultiplier: 2,
-      orientation: "vertical",
-      gestureDirection: "vertical",
-      infinite: false,
-    });
-
-    lenisRef.current = lenis;
-
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-
-    return () => {
-      lenis.destroy();
-      lenisRef.current = null;
-    };
-  }, []);
-
-  useEffect(() => {
-    if (prevPathRef.current !== location.pathname) {
-      if (lenisRef.current) {
-        try {
-          lenisRef.current.scrollTo(0, { duration: 0.8 });
-        } catch (e) {
-          window.scrollTo(0, 0);
-        }
-      } else {
-        window.scrollTo(0, 0);
-      }
-      prevPathRef.current = location.pathname;
-    }
-  }, [location.pathname]);
-
-  return <>{children}</>;
+  return (
+    <ReactLenis root={root} options={lenisOptions} className={className}>
+      {children}
+    </ReactLenis>
+  );
 };
 
 export default SmoothScroll;
