4ed7d0d6835dd0e45efa0e1d0acbb8ad3105fe00
Rakibul hasan <mdrakibulhasan12346@gmail.com>
2026-03-16 11:54:46 +0600

    feat: update color scheme in EventStatusBadge for improved visual consistency

--- src/pages/dashboards/admin/event/components/EventStatusBadge.jsx
+++ src/pages/dashboards/admin/event/components/EventStatusBadge.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 const EventStatusBadge = ({ status }) => {
     switch (status) {
         case 'Featured':
-            return <span className="px-3 py-1 text-sm font-medium text-amber-700 bg-amber-100 rounded-full">Featured</span>;
+            return <span className="px-3 py-1 text-sm font-medium text-[#F97316] bg-[#FFF1E6] rounded-full">Featured</span>;
         case 'Pending':
-            return <span className="px-3 py-1 text-sm font-medium text-orange-600 bg-orange-100 rounded-full">Pending</span>;
+            return <span className="px-3 py-1 text-sm font-medium  text-[#F59E0B] bg-[#FFF3DF] rounded-full">Pending</span>;
         case 'Live':
-            return <span className="px-3 py-1 text-sm font-medium text-teal-700 bg-teal-100 rounded-full">Live</span>;
+            return <span className="px-3 py-1 text-sm font-medium text-[#0F766E] bg-[#E7F1F1] rounded-full">Live</span>;
         case 'Banned':
-            return <span className="px-3 py-1 text-sm font-medium text-red-600 bg-red-100 rounded-full">Banned</span>;
+            return <span className="px-3 py-1 text-sm font-medium text-[#DC2626] bg-[#FEE2E2] rounded-full">Banned</span>;
         case 'Past':
             return <span className="px-3 py-1 text-sm font-medium text-gray-600 bg-gray-100 rounded-full">Past</span>;
         default:
