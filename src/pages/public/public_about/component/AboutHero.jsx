5d3cd8a71e2a4e59f6b3b7a4084a55a60a21facc
Rakibul12356 <mdrakibulhasan12346@gmail.com>
2026-04-15 16:48:55 +0600

    Update AboutHero component with new background images and improved styling

--- src/pages/public/public_about/component/AboutHero.jsx
+++ src/pages/public/public_about/component/AboutHero.jsx
@@ -1,18 +1,54 @@
-﻿import React from 'react';
+﻿// import React from 'react';
+// import Container from '../../../../components/layout/Container';
+// import Button from '../../../../components/ui/Button';
+// import HeroTitle from '../../../../components/ui/HeroTitle';
+
+// const AboutHero = () => {
+//   const backgroundImageUrl = '/images/About.jpg'; 
+//   return (
+//     <div
+//       style={{ backgroundImage: backgroundImageUrl ? `url(${backgroundImageUrl})` : 'none' }}
+//       className="relative flex w-auto items-center justify-center bg-cover bg-center h-70 sm:h-160 md:h-160 lg:h-200"
+//     >
+//       <div className="absolute inset-0 z-0 bg-black opacity-10"></div>
+//       <Container className="relative z-10 py-10 lg:py-0">
+    
+//       </Container>
+//     </div>
+//   );
+// };
+
+// export default AboutHero;
+
+
+
+
+
+
+import React from 'react';
 import Container from '../../../../components/layout/Container';
 import Button from '../../../../components/ui/Button';
 import HeroTitle from '../../../../components/ui/HeroTitle';
 
 const AboutHero = () => {
-  const backgroundImageUrl = '/images/About.jpg'; // Replace with your actual image URL
+
+
   return (
     <div
-      style={{ backgroundImage: backgroundImageUrl ? `url(${backgroundImageUrl})` : 'none' }}
-      className="relative flex w-auto items-center justify-center bg-cover bg-center h-70 sm:h-160 md:h-160 "
+      className={`
+        relative flex w-auto items-center justify-center bg-cover bg-center 
+        h-70 sm:h-160 md:h-160 lg:h-190
+        
+        bg-[url('/images/about1.png')] 
+        
+        lg:bg-[url('/images/About.jpg')]
+      `}
     >
+      {/* Overlay */}
       <div className="absolute inset-0 z-0 bg-black opacity-10"></div>
+      
       <Container className="relative z-10 py-10 lg:py-0">
-    
+        {/* Apnar content ekhane thakbe */}
       </Container>
     </div>
   );
